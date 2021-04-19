5902e8af9be95c6d0904ea1968b090e80de180a6
Yashar Haque <79962551+yasharkhan11@users.noreply.github.com>
2021-04-19 13:39:28 +0530

    Add files via upload

---
+++ GamePlayController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayController : MonoBehaviour
+{
+     public static GamePlayController instance;
+
+     public BoxSpawner box_Spawner;
+      
+     public BoxScript currentBox;
+
+     public CameraFollow cameraScript;
+     private int moveCount;
+
+     void Awake(){
+         if(instance == null)
+            instance =this;
+     }
+
+
+    
+    void Start()
+    {
+         box_Spawner.SpawnBox();
+        
+    }
+
+    void Update()
+    {
+     DetectInput();
+    }
+    void DetectInput(){
+         if(Input.GetMouseButtonDown(0)){
+              currentBox.DropBox();
+         }
+    }
+    public void SpawnNewBox(){
+         Invoke("NewBox",2f);
+
+    }
+    void NewBox(){
+         box_Spawner.SpawnBox();
+    }
+     public void MoveCamera(){
+          moveCount++;
+          if(moveCount == 3){
+               moveCount =0;
+              cameraScript.targetPos.y +=2f; 
+          }
+
+
+     }
+     public void RestartGame(){
+          UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+     }
+
+   
+}
---
+++ ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text hiScoreText;
+    
+    public float score;
+    public float hiScore;
+
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("HiScore"))
+         hiScore = PlayerPrefs.GetFloat("HiScore");
+    }
+
+    void Update()
+    {
+    
+       if(score>hiScore){
+           hiScore = score;
+           PlayerPrefs.SetFloat("HiScore",hiScore);
+       }
+      scoreText.text = Mathf.Round(score).ToString();
+      hiScoreText.text = Mathf.Round(hiScore).ToString();
+    }
+}
